5bf378097bb717da448e5d16ce42873b83c3f708
roynaldi19 <roynaldi19@gmail.com>
2022-09-17 23:18:08 +0700

    service sederhana

--- app/src/main/java/com/roynaldi19/bpai04_01service/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/bpai04_01service/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.roynaldi19.bpai04_01service
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roynaldi19.bpai04_01service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
     }
 }
